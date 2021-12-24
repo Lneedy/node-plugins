6d0e8174781f654d9cf774343a4274bf3f2486ee
Lneedy <1296570868@qq.com>
2021-12-24 16:28:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-gysaew
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-gysaew)
---
+++ index.js
@@ -0,0 +1,3 @@
+// run `node index.js` in the terminal
+console.log(process.versions);
+console.log(`Hello Node.js v${process.versions.node}!`);
